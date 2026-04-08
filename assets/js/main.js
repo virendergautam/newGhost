4f1f5faa8cc6d42233e84fdaff0fb9adfa07f157
virendergautam <virender.gautam@shinedezign.com>
2026-04-08 13:25:55 +0530

    appearance setting made dynamic and more custom setting added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,28 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-    themeToggle()
-  
+  themeToggle();
 });
 
-
 const themeToggle = () => {
-  const toggleBtn = document.getElementById("theme-toggle");
   const html = document.documentElement;
-
-  if (!toggleBtn) return;
-
-  const darkIcon = toggleBtn.querySelector(".icon-dark");
-  const lightIcon = toggleBtn.querySelector(".icon-light");
-
-  // Get saved theme OR default to light
-  let currentTheme = localStorage.getItem("theme") || "light";
-
-  // Apply initial theme
-  html.setAttribute("data-theme", currentTheme);
-
-  if (currentTheme === "dark") {
-    darkIcon.classList.add("hidden");
-    lightIcon.classList.remove("hidden");
+  let customValue = html.getAttribute("data-theme-value");
+  if (customValue === "Dark") {
+    html.setAttribute("data-theme", "dark");
+  } else if (customValue === "Light") {
+    html.setAttribute("data-theme", "light");
   } else {
-    darkIcon.classList.remove("hidden");
-    lightIcon.classList.add("hidden");
-  }
+    const toggleBtn = document.getElementById("theme-toggle");
 
-  toggleBtn.addEventListener("click", () => {
-    // Toggle theme
-    currentTheme = currentTheme === "dark" ? "light" : "dark";
+    if (!toggleBtn) return;
 
-    // Apply
+    const darkIcon = toggleBtn.querySelector(".icon-dark");
+    const lightIcon = toggleBtn.querySelector(".icon-light");
+
+    // Get saved theme OR default to light
+    let currentTheme = localStorage.getItem("theme") || "light";
+
+    // Apply initial theme
     html.setAttribute("data-theme", currentTheme);
-    localStorage.setItem("theme", currentTheme);
 
-    // Update icons
     if (currentTheme === "dark") {
       darkIcon.classList.add("hidden");
       lightIcon.classList.remove("hidden");
@@ -44,5 +30,23 @@ const themeToggle = () => {
       darkIcon.classList.remove("hidden");
       lightIcon.classList.add("hidden");
     }
-  });
+
+    toggleBtn.addEventListener("click", () => {
+      // Toggle theme
+      currentTheme = currentTheme === "dark" ? "light" : "dark";
+
+      // Apply
+      html.setAttribute("data-theme", currentTheme);
+      localStorage.setItem("theme", currentTheme);
+
+      // Update icons
+      if (currentTheme === "dark") {
+        darkIcon.classList.add("hidden");
+        lightIcon.classList.remove("hidden");
+      } else {
+        darkIcon.classList.remove("hidden");
+        lightIcon.classList.add("hidden");
+      }
+    });
+  }
 };
